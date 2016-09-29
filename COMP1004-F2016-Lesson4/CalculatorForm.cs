2303e53081c4e7f294c79c4c0d5258edc6034dc7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-09-29 18:40:55 -0400

    added basic buttons to UI

--- COMP1004-F2016-Lesson4/CalculatorForm.cs
+++ COMP1004-F2016-Lesson4/CalculatorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace COMP1004_F2016_Lesson4
 {
-    public partial class Form1 : Form
+    public partial class CalculatorForm : Form
     {
-        public Form1()
+        public CalculatorForm()
         {
             InitializeComponent();
         }
--- COMP1004-F2016-Lesson4/Program.cs
+++ COMP1004-F2016-Lesson4/Program.cs
@@ -16,7 +16,7 @@ namespace COMP1004_F2016_Lesson4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CalculatorForm());
         }
     }
 }
